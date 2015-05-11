25d4582583b2120f11b27cf6bec3337d1cb4e162
Geoff Kimball <geoff@zurb.com>
2015-05-11 16:07:09 -0700

    Initial commit with folder structure and Sass compiler working

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+
+var sassPaths = [
+  'bower_components/foundation/scss'
+];
+
+gulp.task('sass', function() {
+  return gulp.src('./scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths,
+      errLogToConsole: true
+    }))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', ['sass'], function() {
+  gulp.watch(['./scss/**/*.scss'], ['sass']);
+});
