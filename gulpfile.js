73d38b5faf7db6195b10b039ae3cccde2e119ce9
Geoff Kimball <geoff@zurb.com>
2015-11-16 10:15:57 -0800

    Add Motion UI to dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 
 var sassPaths = [
-  'bower_components/foundation-sites/scss'
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
 ];
 
 gulp.task('sass', function() {
