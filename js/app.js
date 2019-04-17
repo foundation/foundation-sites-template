58a3fe98716ec997cc7da4b7f855e37c9f43642b
Leon <leonfvogler@gmail.com>
2019-04-17 17:42:06 +0200

    defaultspace checkbox members

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 jQuery(document).ready(function () {
 	jQuery(document).foundation();
+
+	$('.user input[type=checkbox]').on('change', function() {
+		$(this).parents('.user').toggleClass('is-active');
+	});
 });
