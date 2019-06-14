5279d63367e83e9e060438d1e328aeb9537382ef
Leon <leonfvogler@gmail.com>
2019-06-14 13:43:48 +0200

    user select defaultspace

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
 jQuery(document).ready(function () {
 	jQuery(document).foundation();
-
-	jQuery(document).on('change', '.user input[type=checkbox]', function() {
-		jQuery(this).parents('.user').toggleClass('is-active');
-	});
 });
