7930ebabad4cd95d8fdf58b1968e8c51e95587c6
Leon <leonfvogler@gmail.com>
2019-06-25 17:14:07 +0200

    space-privacy

--- js/app.js
+++ js/app.js
@@ -3,4 +3,9 @@ jQuery(document).ready(function () {
 	jQuery(document).on('click', '.dots', function(e) {
 		e.preventDefault();
 	});
+	jQuery(document).on('change', '.radio-accordion .radio-input', function(e) {
+		let name = $(this).attr('name');
+		$('.radio-accordion input[name="' + name + '"]').parents('.radio-accordion-item').find('.radio-accordion-content').slideUp(250);
+		$(this).parents('.radio-accordion-item').find('.radio-accordion-content').slideDown(250);
+	});
 });
