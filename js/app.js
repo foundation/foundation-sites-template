b1252797eecfd2fc0f80a80a6037d1db761ab006
Leon <leonfvogler@gmail.com>
2019-06-27 02:04:29 +0200

    disabled check/radio/switch; auto-expand textarea

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 jQuery(document).ready(function () {
-	jQuery(document).foundation();
-	jQuery(document).on('click', '.dots', function(e) {
+	$(document).foundation();
+	$(document).on('click', '.dots', function(e) {
 		e.preventDefault();
 	});
-	jQuery(document).on('change', '.radio-accordion .radio-input', function(e) {
+	$(document).on('change', '.radio-accordion .radio-input', function(e) {
 		let name = $(this).attr('name');
 		$('.radio-accordion input[name="' + name + '"]').parents('.radio-accordion-item').find('.radio-accordion-content').slideUp(250);
 		$(this).parents('.radio-accordion-item').find('.radio-accordion-content').slideDown(250);
+	});
+
+	$(document)
+	.one('focus.auto-expand', 'textarea.auto-expand', function(){
+		var savedValue = this.value;
+		this.value = '';
+		this.baseScrollHeight = this.scrollHeight;
+		this.value = savedValue;
+	})
+	.on('input.auto-expand', 'textarea.auto-expand', function(){
+		var minRows = this.getAttribute('data-min-rows')|0, rows;
+		var lineHeight = parseInt($(this).css('line-height'), 10);
+		this.rows = minRows;
+		rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / lineHeight);
+		this.rows = minRows + rows;
 	});
 });
