d42c532bff7c004cbd5a2950697ae2880a8e2d42
Leon <leonfvogler@gmail.com>
2019-06-27 02:45:07 +0200

    post-card hover

--- js/app.js
+++ js/app.js
@@ -8,19 +8,24 @@ jQuery(document).ready(function () {
 		$('.radio-accordion input[name="' + name + '"]').parents('.radio-accordion-item').find('.radio-accordion-content').slideUp(250);
 		$(this).parents('.radio-accordion-item').find('.radio-accordion-content').slideDown(250);
 	});
-
-	$(document)
-	.one('focus.auto-expand', 'textarea.auto-expand', function(){
+	$(document).one('focus.auto-expand', 'textarea.auto-expand', function(){
 		var savedValue = this.value;
 		this.value = '';
 		this.baseScrollHeight = this.scrollHeight;
 		this.value = savedValue;
-	})
-	.on('input.auto-expand', 'textarea.auto-expand', function(){
+	}).on('input.auto-expand', 'textarea.auto-expand', function(){
 		var minRows = this.getAttribute('data-min-rows')|0, rows;
 		var lineHeight = parseInt($(this).css('line-height'), 10);
 		this.rows = minRows;
 		rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / lineHeight);
 		this.rows = minRows + rows;
 	});
+	$(document).on('mouseenter', '[data-touch-toggle]', function() {
+		let target = $('#' + $(this).attr('data-touch-toggle'));
+		target.addClass(target.attr('data-touch-toggler'));
+	}).on('mouseleave', '[data-touch-toggle]', function(e) {
+		let target = $('#' + $(this).attr('data-touch-toggle'));
+		target.removeClass(target.attr('data-touch-toggler'));
+	});
+
 });
