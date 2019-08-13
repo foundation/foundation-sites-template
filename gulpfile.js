b369cbba0b1efa2b5cd0d9ad6433d6d5eafbf992
tivus <mackbird@gmail.com>
2019-08-13 10:30:16 +0200

    add sourcemaps. reinstall node modules via "npm install"

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp          = require('gulp');
 var browserSync   = require('browser-sync').create();
 var $             = require('gulp-load-plugins')();
 var autoprefixer  = require('autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 var sassPaths = [
   'node_modules/foundation-sites/scss',
@@ -9,7 +10,8 @@ var sassPaths = [
 ];
 
 function sass() {
-  return gulp.src('scss/app.scss')
+	return gulp.src('scss/app.scss')
+		.pipe(sourcemaps.init())
     .pipe($.sass({
       includePaths: sassPaths,
       outputStyle: 'compressed' // if css compressed **file size**
@@ -17,7 +19,8 @@ function sass() {
       .on('error', $.sass.logError))
     .pipe($.postcss([
       autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] })
-    ]))
+		]))
+		.pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('css'))
     .pipe(browserSync.stream());
 };
