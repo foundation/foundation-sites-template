b32edf8f4a3ebcc2b43caf82a555d568531dc704
schuhwerk <vitus.schuhwerk@gmail.com>
2020-07-06 18:23:28 +0200

    feat: :zap: local include roboto

--- js/app.js
+++ js/app.js
@@ -1,13 +1,8 @@
 /**
  * todo self host the fonts: https://google-webfonts-helper.herokuapp.com/
  */
-jQuery.getScript("https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js", function () {
-	WebFont.load({
-		google: {
-			families: ['PT+Sans:400,700', 'Roboto:400,700']
-		}
-	});
-});
+
+
 !function( window, document, $ ){
 
 	$(document).ready(function() {
