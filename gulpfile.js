28c4b7c2114d8472aaeb4a1537b11562297ee8d1
Stephan von Lingelsheim <dev@vonlingelsheim.com>
2023-01-11 01:33:50 +0100

    hompeage

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,36 @@
-var gulp          = require('gulp');
-var browserSync   = require('browser-sync').create();
-var $             = require('gulp-load-plugins')();
-var autoprefixer  = require('autoprefixer');
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var $ = require("gulp-load-plugins")();
+var autoprefixer = require("autoprefixer");
 
 var sassPaths = [
-  'node_modules/foundation-sites/scss',
-  'node_modules/motion-ui/src'
+  "node_modules/foundation-sites/scss",
+  "node_modules/motion-ui/src",
 ];
 
 function sass() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest('css'))
+  return gulp
+    .src("scss/app.scss")
+    .pipe(
+      $.sass({
+        includePaths: sassPaths,
+        outputStyle: "compressed", // if css compressed **file size**
+      }).on("error", $.sass.logError)
+    )
+    .pipe($.postcss([autoprefixer()]))
+    .pipe(gulp.dest("css"))
     .pipe(browserSync.stream());
-};
+}
 
 function serve() {
   browserSync.init({
-    server: "./"
+    server: "./",
   });
 
   gulp.watch("scss/*.scss", sass);
-  gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch("*.html").on("change", browserSync.reload);
 }
 
-gulp.task('sass', sass);
-gulp.task('serve', gulp.series('sass', serve));
-gulp.task('default', gulp.series('sass', serve));
+gulp.task("sass", sass);
+gulp.task("serve", gulp.series("sass", serve));
+gulp.task("default", gulp.series("sass", serve));
