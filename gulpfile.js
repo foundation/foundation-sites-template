3dbc9fb0a37b07dc3f93b52163b07d9aaa940f32
Joe Workman <joe@workmanmail.com>
2023-08-18 12:08:33 -0700

    updated package to reflect 6.8.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,30 @@
-var gulp          = require('gulp');
-var browserSync   = require('browser-sync').create();
-var $             = require('gulp-load-plugins')();
-var autoprefixer  = require('autoprefixer');
+const gulp         = require('gulp');
+const sass         = require('gulp-sass')(require('sass'));
+const browserSync  = require('browser-sync').create();
+const sourcemaps   = require('gulp-sourcemaps');
 
-var sassPaths = [
-  'node_modules/foundation-sites/scss',
-  'node_modules/motion-ui/src'
+const includePaths = [
+    'node_modules/foundation-sites/scss',
+    'node_modules/motion-ui/src'
 ];
 
-function sass() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.stream());
+function sassBuild() {
+    return gulp.src(['scss/app.scss'])
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths : includePaths,
+            outputStyle  : 'compressed'
+        }).on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('css'));
 };
 
 function serve() {
-  browserSync.init({
-    server: "./"
-  });
-
-  gulp.watch("scss/*.scss", sass);
-  gulp.watch("*.html").on('change', browserSync.reload);
+    browserSync.init({server : "./"});
+    gulp.watch("scss/*.scss", sassBuild);
+    gulp.watch("*.html").on('change', browserSync.reload);
 }
 
-gulp.task('sass', sass);
+gulp.task('sass', sassBuild);
 gulp.task('serve', gulp.series('sass', serve));
 gulp.task('default', gulp.series('sass', serve));
